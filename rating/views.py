d9f5be0601ff8efeda80c9edb4ea51e17650bd64
Jaccojwang <danolago@gmail.com>
2019-05-23 11:26:53 +0300

    Create index.html and index view function

--- rating/views.py
+++ rating/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 
-
+@login_required(login_url='/accounts/login')
 def index(request):
-    pass
+    current_user = request.user
+    projects = Projects.objects.all()
+    return render(request, 'index.html', locals())
 
+@login_required(login_url='/accounts/login')
 def profile(request):
     pass
 
+@login_required(login_url='/accounts/login')
 def edit_profile(request):
     pass
 
+@login_required(login_url='/accounts/login')
 def post(request):
     pass
 
+@login_required(login_url='/accounts/login')
 def project(request):
     pass
 
+@login_required(login_url='/accounts/login')
 def search(request):
     pass
