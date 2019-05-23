e9eb3177c49a6c3a3f84ef56ba65607d37134ad7
Jaccojwang <danolago@gmail.com>
2019-05-23 12:31:09 +0300

    Create edit_profile view

--- rating/views.py
+++ rating/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Project, Profile, Rating, categories, technologies
+from .forms import ProfileForm, UploadForm
 
 @login_required(login_url='/accounts/login')
 def index(request):
@@ -20,7 +21,18 @@ def profile(request):
 
 @login_required(login_url='/accounts/login')
 def edit_profile(request):
-    pass
+    current_user = request.user
+    profile=Profile.objects.filter(user=current_user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            prof = form.save(commit=False)
+            prof.user = current_user
+            prof.save()
+            return redirect('edit_profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'edit_profile.html', {'form': form})
 
 @login_required(login_url='/accounts/login')
 def new_project(request):
