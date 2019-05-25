d4aa2e7e0dccf7730f2c6c4be842cdd2364b1f96
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-05-25 11:10:00 -0400

    Create views for getting API data

--- rating/views.py
+++ rating/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from .models import Project, Profile, Rating, categories, technologies
 from .forms import ProfileForm, UploadForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer, ProjectSerializer
+
 
 @login_required(login_url='/accounts/login')
 def index(request):
@@ -65,3 +69,15 @@ def search(request):
     else:
         message = "Please enter search term"
         return render(request, 'search,html', {"message":message, "projects":projects})
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
