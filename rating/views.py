75df188478f263a8e8e2d7d371062adb37ebc089
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-05-26 10:07:31 -0400

    Fine tune rating to work properly

--- rating/views.py
+++ rating/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from .models import Project, Profile, Rating, categories, technologies
-from .forms import ProfileForm, UploadForm
+from .forms import ProfileForm, UploadForm, RatingForm
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import ProfileSerializer, ProjectSerializer
@@ -54,13 +54,52 @@ def new_project(request):
 
 @login_required(login_url='/accounts/login')
 def project(request, project_id):
+    current_user = request.user
+    profile =Profile.objects.get(user=current_user)
+    message = "Thank you for voting"
     try:
         project = Project.objects.get(id=project_id)
-        # if user has voted on a project(you have voted on this project before) ask if he wants to reasess...if yes delete previous and take in new
-        # each 
     except Project.DoesNotExist:
-        raise Http404()
-    return render(request, "project.html", locals())
+        raise ObjectDoesNotExist()
+
+    try:
+        ratings = Rating.objects.filter(project=project_id)
+        design = list(Rating.objects.filter(project=project_id).values_list('design',flat=True))
+        usability = list(Rating.objects.filter(project=project_id).values_list('usability',flat=True))
+        creativity = list(Rating.objects.filter(project=project_id).values_list('creativity',flat=True))
+        content = list(Rating.objects.filter(project=project_id).values_list('content',flat=True))
+        
+        total_design=sum(design)
+        total_usability=sum(usability)
+        total_creativity=sum(creativity)
+        total_content=sum(content)
+
+
+        overall_score=total_design+total_content+total_usability+total_creativity
+
+        project.design = total_design
+        project.usability = total_usability
+        project.creativity = total_creativity
+        project.content = total_content
+        project.overall_score = overall_score
+
+        project.save()
+
+    except:
+        return None
+    
+    if request.method == 'POST':
+        form = RatingForm(request.POST, request.FILES) 
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.project= project
+            rating.profile = profile
+            if not Rating.objects.filter(profile=profile, project=project).exists():
+                rating.overall_score = (rating.design+rating.usability+rating.creativity+rating.content)/4
+                rating.save()
+    else:
+        form = RatingForm()
+    return render(request, "project.html",{"project":project,"profile":profile,"ratings":ratings,"form":form, "message":message})
 
 @login_required(login_url='/accounts/login')
 def search(request):
@@ -72,7 +111,7 @@ def search(request):
         return render(request, 'search.html', {"message":message, "projects":projects})
     else:
         message = "Please enter search term"
-        return render(request, 'search,html', {"message":message, "projects":projects})
+        return render(request, 'search.html', {"message":message, "projects":projects})
 
 class ProfileList(APIView):
     permission_classes = (IsAdminOrReadOnly,)
@@ -101,7 +140,7 @@ class ProjectList(APIView):
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileDescription(APIView):
-    permission_classes = (IsAdminOrReadOnly,)
+
     def get_profile(self, pk):
         try:
             return Profile.objects.get(pk=pk)
@@ -114,7 +153,7 @@ class ProfileDescription(APIView):
         return Response(serializers.data)
 
 class ProjectDescription(APIView):
-    permission_classes = (IsAdminOrReadOnly,)
+    
     def get_project(self, pk):
         try:
             return Project.objects.get(pk=pk)
